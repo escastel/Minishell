24c648c4b142dc2e22f2ae9e8055b0672bb9491a
100tfko <148204269+100tfko@users.noreply.github.com>
2024-03-28 13:19:41 +0100

    Add files via upload
    Actualizando lexer

--- lexer.c
+++ lexer.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   lexer.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: escastel <escastel@42.fr>                  +#+  +:+       +#+        */
+/*   By: lcuevas- <lcuevas-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/26 12:38:48 by escastel          #+#    #+#             */
-/*   Updated: 2024/03/26 17:48:50 by escastel         ###   ########.fr       */
+/*   Updated: 2024/03/28 09:47:30 by lcuevas-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	ft_singlequote(char **argv, char **str, int j)
 	(*str)[j] = **argv;
 	*argv += 1;
 	j += 1;
-	while (**argv != 39)
+	while (**argv != '\'')
 	{
 		(*str)[j] = **argv;
 		*argv += 1;
@@ -34,7 +34,7 @@ int	ft_doublequote(char **argv, char **str, int j)
 	(*str)[j] = **argv;
 	*argv += 1;
 	j += 1;
-	while (**argv != '"')
+	while (**argv != '\"')
 	{
 		(*str)[j] = **argv;
 		*argv += 1;
@@ -69,9 +69,9 @@ char	*ft_take_first_word(char **argv)
 	while (**argv != ' ' && **argv)
 	{
 		j = ft_backlashes(&(*argv), &str, j);
-		if (**argv == '"')
+		if (**argv == '\"')
 			j = ft_doublequote(&(*argv), &str, j);
-		if (**argv == 39)
+		if (**argv == '\'')
 			j = ft_singlequote(&(*argv), &str, j);
 		if (**argv != ' ')
 		{
@@ -84,17 +84,15 @@ char	*ft_take_first_word(char **argv)
 	return (str);
 }
 
-t_cmds	*ft_fill_command(char *line)
+void	lexer(t_data *data, char *line)
 {
-	t_cmds	*command;
-	char	*aux;
 	int		i;
-	int		n;
+	char	*aux;
+	t_cmds	*command;
 
 	i = 0;
-	n = ft_strlen(line);
 	command = ft_calloc(1, sizeof(t_cmds));
-	command->full_cmd = ft_calloc(1, 10*(sizeof(char **)));
+	command->full_cmd = ft_calloc(1, 10*(sizeof(char **))); // problema del tamano de esto
 	while (ft_strlen(line) != 0)
 	{
 		aux = ft_take_first_word(&line);
@@ -102,28 +100,6 @@ t_cmds	*ft_fill_command(char *line)
 		command->full_cmd[i] = aux;
 		i += 1;
 	}
-	return (command);
-}
-
-void	lexer(t_data *data, char *line)
-{
-	t_cmds	*command;
-	int		i;
-
-	command = ft_fill_command(line);
 	data->cmd[0] = command;
-	printf("%s\n", "printCOMMAND");
-	i = 0;
-	while (command->full_cmd[i])
-	{
-		printf("%s\n", command->full_cmd[i]);
-		i += 1;
-	}
-	printf("%s\n", "printDATA->CMD");
-	i = 0;
-	while (data->cmd[0]->full_cmd[i])
-	{
-		printf("%s\n", data->cmd[0]->full_cmd[i]);
-		i += 1;
-	}
 }
+// el segmentation lo da con el printf, es decir, rellena pero mete mierda al final
