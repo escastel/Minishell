2b3971d219b7d2b21a63430ba3b2adddbd130672
Ester Castellano Rios <escastel@c2r3s4.42malaga.com>
2024-04-10 18:18:14 +0200

    cambio a auxiliar

--- parser.c
+++ parser.c
@@ -3,28 +3,28 @@
 /*                                                        :::      ::::::::   */
 /*   parser.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lcuevas- <lcuevas-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: escastel <escastel@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/27 10:56:36 by lcuevas-          #+#    #+#             */
-/*   Updated: 2024/04/10 10:47:45 by lcuevas-         ###   ########.fr       */
+/*   Updated: 2024/04/10 18:16:38 by escastel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	ft_command_filter(t_data *data)
+int	ft_command_filter(t_data *data, t_list *cmd)
 {
 	int		i;
 	char	*cmd_slash;
 	char	*tmp;
 
 	i = 0;
-	cmd_slash = ft_strjoin("/", ((t_cmds *)data->cmd->content)->full_cmd[0]);
+	cmd_slash = ft_strjoin("/", ((t_cmds *)cmd->content)->full_cmd[0]);
 	while (data->cmd_path[i])
 	{
 		tmp = ft_strjoin(data->cmd_path[i], cmd_slash);
-		((t_cmds *)data->cmd->content)->exc_path = tmp;
-		if (access(((t_cmds *)data->cmd->content)->exc_path, X_OK) == 0)
+		((t_cmds *)cmd->content)->exc_path = tmp;
+		if (access(((t_cmds *)cmd->content)->exc_path, X_OK) == 0)
 			return (0);
 		free(tmp);
 		i += 1;
@@ -53,7 +53,7 @@ void	ft_path(t_data *data)
 	data->cmd_path = ft_split (envp_path, ':');
 }
 
-void	ft_execute(t_data *data)
+void	ft_execute(t_data *data, t_list	*cmd)
 {
 	pid_t	pid;
 
@@ -64,7 +64,7 @@ void	ft_execute(t_data *data)
 	{
 		close(data->pipe[0]);
 		dup2(data->pipe[1], STDOUT_FILENO);
-		if (execve(((t_cmds *)data->cmd->content)->exc_path, ((t_cmds *)data->cmd->content)->full_cmd, data->env) == -1)
+		if (execve(((t_cmds *)cmd->content)->exc_path, ((t_cmds *)cmd->content)->full_cmd, data->env) == -1)
 			exit(EXIT_FAILURE);
 	}
 	else
@@ -75,7 +75,7 @@ void	ft_execute(t_data *data)
 	}
 }
 
-void	ft_execute_last(t_data *data)
+void	ft_execute_last(t_data *data, t_list *cmd)
 {
 	pid_t	pid;
 
@@ -85,8 +85,8 @@ void	ft_execute_last(t_data *data)
 	if (pid == 0)
 	{
 		close (data->pipe[0]);
-		dup2(((t_cmds *)data->cmd->content)->outfile, STDOUT_FILENO);
-		if (execve(((t_cmds *)data->cmd->content)->exc_path, ((t_cmds *)data->cmd->content)->full_cmd, data->env) == -1)
+		dup2(((t_cmds *)cmd->content)->outfile, STDOUT_FILENO);
+		if (execve(((t_cmds *)cmd->content)->exc_path, ((t_cmds *)cmd->content)->full_cmd, data->env) == -1)
 			exit(EXIT_FAILURE);
 	}
 	else
@@ -97,23 +97,27 @@ void	ft_execute_last(t_data *data)
 
 void	parser(t_data *data)
 {
+	t_list	*aux;
 	int		i;
 
 	i = 0;
 	ft_path(data);
-	while (data->cmd->next)
+	aux = data->cmd;
+	while (aux->next)
 	{
 		pipe(data->pipe);
 	//LAS REDIRECCIONES ESTAN DENTRO DEL EXECUTE NO AFECTA AL BUILTIN, SE PODRIAN sacar? o meter el builtin dentro con boolean
 		builtins_control(data, ((t_cmds *)data->cmd->content)->full_cmd); //boolean?
-		if (ft_command_filter(data) == 0)
+		if (ft_command_filter(data, aux) == 0)
 		{
-			ft_execute(data);
+			ft_execute(data, aux);
 		}
-		data->cmd = data->cmd->next;
+		printf("%s\n", ((t_cmds *)data->cmd->content)->full_cmd[0]);
+		printf("%s\n", ((t_cmds *)aux->content)->full_cmd[0]);
+		aux = aux->next;
 	}
 	builtins_control(data, ((t_cmds *)data->cmd->content)->full_cmd);
-	if (ft_command_filter(data) == 0)
-		ft_execute_last(data);
+	if (ft_command_filter(data, aux) == 0)
+		ft_execute_last(data, aux);
 	dup2(((t_cmds *)data->cmd->content)->infile, STDIN_FILENO);
 }
