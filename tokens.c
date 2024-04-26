5c6179c52a2f89f0f57cc01b90fe4b69d55e6853
Ester Castellano Rios <escastel@c2r5s3.42malaga.com>
2024-04-26 15:24:34 +0200

    arreglo de tokens

--- tokens.c
+++ tokens.c
@@ -6,59 +6,63 @@
 /*   By: escastel <escastel@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/26 12:53:17 by escastel          #+#    #+#             */
-/*   Updated: 2024/04/26 13:08:54 by escastel         ###   ########.fr       */
+/*   Updated: 2024/04/26 15:20:57 by escastel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static int	token_utils(char **line, char **str, int *i)
+static int	token_utils(char **line, char **tmp, int *i)
 {
-	*str[*i] = **line;
+	char	*str;
+
+	str = ft_calloc(1, ft_strlen(*line) + 1);
+	str[*i] = **line;
 	*line += 1;
 	*i += 1;
-	if (**line == *str[*i - 1])
+	if (**line == str[*i - 1])
 	{
-		*str[*i] = **line;
+		str[*i] = **line;
 		*line += 1;
 		*i += 1;
 		while (**line && (**line == 32 || (**line >= 9 && **line <= 13)))
 			*line += 1;
-		if (**line == *str[*i - 1] || **line == '\0')
-		{
-			error_msg("michishell: syntax error near unexpected token");
-			free (*str);
-			*str = NULL;
-			return (1);
-		}
-	}
-	while (**line && (**line == 32 || (**line >= 9 && **line <= 13)))
-		*line += 1;
-	return (0);
-}
-
-int	ft_tokens_mayor(char **line, char **tmp)
-{
-	char	*str;
-	int		i;
-
-	i = 0;
-	str = ft_calloc(1, ft_strlen(*line));
-	if (**line == '>')
-	{
-		if (token_utils(line, &str, &i))
-			return (1);
-		if (**line == '<' || **line == '|' || **line == '\0')
+		if (**line == str[*i - 1] || **line == '\0')
 		{
 			error_msg("michishell: syntax error near unexpected token");
 			free (str);
 			str = NULL;
 			return (1);
 		}
-		str[i] = '\0';
-		*tmp = ft_strdup(str);
-		free (str);
-		str = NULL;
+	}
+	while (**line && (**line == 32 || (**line >= 9 && **line <= 13)))
+		*line += 1;
+	*tmp = ft_strdup(str);
+	return (0);
+}
+
+int	ft_tokens_mayor(char **line, char **tmp)
+{
+	int		i;
+
+	i = 0;
+	if (**line == '>')
+	{
+		if (token_utils(line, tmp, &i))
+		{
+			if (tmp)
+				free (tmp);
+			tmp = NULL;
+			return (1);
+		}
+		if (**line == '<' || **line == '|' || **line == '\0')
+		{
+			error_msg("michishell: syntax error near unexpected token");
+			if (tmp)
+				free (tmp);
+			tmp = NULL;
+			return (1);
+		}
 		return (2);
 	}
 	return (0);
@@ -66,26 +70,26 @@ int	ft_tokens_mayor(char **line, char **tmp)
 
 int	ft_tokens_minor(char **line, char **tmp)
 {
-	char	*str;
 	int		i;
 
 	i = 0;
-	str = ft_calloc(1, ft_strlen(*line));
 	if (**line == '<')
 	{
-		if (token_utils(line, &str, &i))
+		if (token_utils(line, tmp, &i))
+		{
+			if (tmp)
+				free (tmp);
+			tmp = NULL;
 			return (1);
+		}
 		if (**line == '>' || **line == '|' || **line == '\0')
 		{
 			error_msg("michishell: syntax error near unexpected token");
-			free (str);
-			str = NULL;
+			if (tmp)
+				free (tmp);
+			tmp = NULL;
 			return (1);
 		}
-		str[i] = '\0';
-		*tmp = ft_strdup(str);
-		free (str);
-		str = NULL;
 		return (2);
 	}
 	return (0);
